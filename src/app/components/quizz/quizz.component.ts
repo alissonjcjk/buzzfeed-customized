5cc00bb6c0eeefbff72687bd117d10522568d6df
Alisson Bernadino <alisson.bernardino@transportes.gov.br>
2026-04-20 21:32:59 -0300

    feat: aplica design premium glassmorphism e animacoes

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+  title: string = 'Você seria um herói ou vilão?';
+  
+  questionSelected: any = {
+    question: 'Qual super-poder você escolheria?',
+    options: [
+      { name: 'Raios-Lasers' },
+      { name: 'Voar' },
+      { name: 'Explodir coisas' },
+      { name: 'Curar feridas' }
+    ]
+  };
 
+  finished: boolean = false;
+  answerSelected: string = 'Você muito provavelmente seria um super Herói!';
 }
